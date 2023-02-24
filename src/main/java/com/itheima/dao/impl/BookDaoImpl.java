5f41542bc7d06dc773ca1fcd9a41f7da835d1073
ViceWong <2803107606@qq.com>
2023-02-24 16:23:46 +0800

    Manage-Class

--- src/main/java/com/itheima/dao/impl/BookDaoImpl.java
+++ src/main/java/com/itheima/dao/impl/BookDaoImpl.java
@@ -7,10 +7,14 @@ import com.itheima.dao.BookDao;
 -------------------------构造器注入---------------------------------------------------------------
  */
 public class BookDaoImpl implements BookDao {
+    private String hah;
 
+    public void setHah(String hah) {
+        this.hah = hah;
+    }
 
     public void save() {
-        System.out.println("this is bookDao......." );
+        System.out.println("this is bookDao......." + hah);
     }
 
 }
